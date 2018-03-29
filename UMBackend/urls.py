d4148d9347113c6ed0241d83385bec4014635f06
Xuong <xuong.trantrieu@amaris.com>
2018-03-29 16:15:00 +0700

    Add homepage

--- UMBackend/urls.py
+++ UMBackend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import api_root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root)
 ]
