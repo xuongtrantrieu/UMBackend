687f9af1430c2959d71d48099c9b5b2b18039ccb
Xuong <xuong.trantrieu@amaris.com>
2018-03-29 16:06:07 +0700

    Fix admin editing individual user

--- addons/core/users/admin.py
+++ addons/core/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name']
+    ordering = ['first_name']
+
+admin.site.register(User, UserAdmin)
