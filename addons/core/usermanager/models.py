e7e6f307ba804fedf895c53bf860f26e225f6377
Xuong <xuong.trantrieu@amaris.com>
2018-03-29 15:33:53 +0700

    Fix UserManager

--- addons/core/usermanager/models.py
+++ addons/core/usermanager/models.py
@@ -6,21 +6,19 @@ class UserManager(BaseUserManager):
     A customer user manager to deal with emails as unique identifiers for auth instead of usernames.
     """
 
-    @classmethod
-    def _create_user(cls, email, password, ** kwargs):
+    def _create_user(self, email, password, **kwargs):
         """
         Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError('The email must be set')
-        email = cls.normalize_email(email)
-        user = cls.model(email=email, ** kwargs)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
         user.set_password(password)
         user.save()
         return user
 
-    @classmethod
-    def create_superuser(cls, email, password, ** kwargs):
+    def create_superuser(self, email, password, **kwargs):
         kwargs.setdefault('is_staff', True)
         kwargs.setdefault('is_superuser', True)
         kwargs.setdefault('is_active', True)
